df5e671f23e74238a852c9e5d27d664b87266caf
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2017-07-20 12:02:46 +0200

    Fix JS bootstrap

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -23,7 +23,7 @@ axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 const token = document.head.querySelector('meta[name="csrf-token"]');
 
 if (token) {
-    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+    axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
 } else {
     console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
 }
