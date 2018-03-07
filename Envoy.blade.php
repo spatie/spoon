f8d19716540d4f46ff1018cc695b8fbb2930006f
Willem Van Bockstal <willem@spatie.be>
2018-03-07 16:44:46 +0100

    Update Envoy.blade.php

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -125,7 +125,7 @@ php artisan config:clear
 php artisan cache:clear
 php artisan config:cache
 
-sudo service php7.1-fpm restart
+sudo service php7.2-fpm restart
 sudo supervisorctl restart all
 @endtask
 
@@ -149,5 +149,5 @@ php artisan config:clear
 php artisan cache:clear
 php artisan config:cache
 sudo supervisorctl restart all
-sudo service php7.1-fpm restart
+sudo service php7.2-fpm restart
 @endtask
